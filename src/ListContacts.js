2e0ccaeafe3722e8c13d7d770ffa1a8d0e782ea3
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-05-19 18:03:08 +0530

    Added Search bar with state updates.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,37 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-function ListContacts (props){
-    return (
-        <ol className="contact-list">
-            {props.contacts.map((contact) => (
-                <li key={contact.id} className='contact-list-item'>
-                    <div 
-                        className='contact-avatar'
-                        style = {{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}
-                    ></div>
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
 
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.handle}</p>
-                    </div>
+    state = {
+        query: ""
+    }
 
-                    <button
-                        onClick = {() => props.onDeleteContact(contact)}
-                        className='contact-remove'>
-                            Remove
-                    </button>
-                </li>
-            ))}
-        </ol>
-    )
-}
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
 
-ListContacts.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    onDeleteContact: PropTypes.func.isRequired
+    render(){
+        return(
+            <div className="list-contacts">
+                <div className="list-contacts-top">
+                    <input
+                        className="search-contacts"
+                        type="text"
+                        placeholder="Search Contacts"
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+                <ol className="contact-list">
+                    {this.props.contacts.map((contact) => (
+                        <li key={contact.id} className='contact-list-item'>
+                            <div 
+                                className='contact-avatar'
+                                style = {{
+                                    backgroundImage: `url(${contact.avatarURL})`
+                                }}
+                            ></div>
+
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+
+                            <button
+                                onClick = {() => this.props.onDeleteContact(contact)}
+                                className='contact-remove'>
+                                    Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
 }
 
 export default ListContacts
