d01c2b6dd9c92f9b08b12e3b833b2099c0e8d4c4
Hanumanth P <48819979+vildar@users.noreply.github.com>
2020-05-20 15:03:58 +0530

    Added React Router components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 class ListContacts extends Component {
     static propTypes = {
@@ -41,6 +42,10 @@ class ListContacts extends Component {
                         value={this.state.query}
                         onChange={(event) => this.updateQuery(event.target.value)}
                     />
+                    <Link 
+                        to='/create'
+                        className = 'add-contact'
+                    >Add Contact</Link>
                 </div>
 
                 { showContacts !== contacts && (
